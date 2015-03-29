d96cba7f45275cf57c1de7019a426dbb24ba3bd0
EWa74 <eric.wahl@web.de>
2015-03-29 21:36:04 +0200

    1st upload to git

---
+++ HalloWelt/src/HalloWelt.java
@@ -0,0 +1,14 @@
+
+public class HalloWelt {
+
+	public static void main(String[] args) {
+		Bank meineBank = new Bank();
+		meineBank.kunde = new Kunde[1000];
+		meineBank.kunde[0] = new Kunde();
+		meineBank.kunde[0].konto = new Konto();
+		Konto konto = meineBank.kunde[0].konto;
+		konto.nummer=123;
+		System.out.println(konto.nummer);
+	}
+
+}
