7d271c630139bf05d26b47bd8de9452d7bf5ae4c
EWa74 <Eric.Wahl@web.de>
2015-04-13 22:05:30 +0200

    Ende Kapitel 6.7

--- HalloWelt/src/de/java2enterprise/onlinebanking/Onlinebanking.java
+++ HalloWelt/src/de/java2enterprise/onlinebanking/Onlinebanking.java
@@ -1,18 +1,22 @@
 package de.java2enterprise.onlinebanking;
 
-import de.java2enterprise.onlinebanking.model.bank.Bank;
+import java.util.List;
+import java.util.ArrayList;
+
 import de.java2enterprise.onlinebanking.model.kunde.GeschäftsKunde;
+import de.java2enterprise.onlinebanking.model.kunde.Kunde;
 
       
 public class Onlinebanking extends Object {
 
 	public static void main(String[] args) {
 		
-		Bank<GeschäftsKunde> bank = new Bank<GeschäftsKunde>();
-		GeschäftsKunde[] kunden = bank.getE();
-		kunden = new GeschäftsKunde[1000];
-		kunden[0] = new GeschäftsKunde(123);
-		System.out.println(kunden[0]);
+		List<Kunde> bank = new ArrayList<Kunde>();
+		bank.add(new GeschäftsKunde(123));
+		bank.add(new GeschäftsKunde(456));
+		for (Kunde kunde : bank){
+			System.out.println(kunde);
+		}
 		
 	}
 }
