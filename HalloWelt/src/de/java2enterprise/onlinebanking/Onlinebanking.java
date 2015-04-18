a7278d8dd84ca69db901b7c98e1b60c6e3fa7389
EWa74 <Eric.Wahl@web.de>
2015-04-18 22:21:03 +0200

    Ende Kapitel 6.13

--- HalloWelt/src/de/java2enterprise/onlinebanking/Onlinebanking.java
+++ HalloWelt/src/de/java2enterprise/onlinebanking/Onlinebanking.java
@@ -1,13 +1,26 @@
 package de.java2enterprise.onlinebanking;
 
-import java.util.function.Consumer;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Onlinebanking 
 {
 
 	public static void main(String[] args) 
 	{
-		Consumer<String> c= Printer::new;
-		c.accept("Hallo Welt"); 
+		List<Member> m = Member.getAllMembers();
+		
+		Map<Integer, List<Member>> grouped = m.stream().collect(
+				Collectors.groupingBy(Member::getTeam)
+				);
+		
+		Collection<List<Member>> c = grouped.values();
+		for (List<Member> member :c) {
+			member.stream().forEach(
+					a -> System.out.println(a.getTeam()+": " + a.getName())
+					);
+		}
 	}
 } 
