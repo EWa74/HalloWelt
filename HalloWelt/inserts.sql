c787754f6240b741b932e9c2692b476e3ed3f54d
EWa74 <eric.wahl@web.de>
2015-04-22 17:15:39 +0200

    Ende Kapitel 8.4 Einen JDBC Treiber installieren

---
+++ HalloWelt/inserts.sql
@@ -0,0 +1,7 @@
+insert into kunde (id, email, password)
+values (1, 'janssen@java2enterprise.de', 'Taxi#750');
+
+insert into kunde (id, email, password)
+values (2, 'mueller@java2enterprise.de', 'Kiosk#0815');
+
+commit;
